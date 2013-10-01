630be9094b18408fa7f3ea252d41419eff68872a
Rick Walsh <Rick.Walsh@gmail.com>
2013-10-01 13:34:04 +0100

    first iteration of erosion

--- Assets/Scripts/TerrainHeightMapBuilder.cs
+++ Assets/Scripts/TerrainHeightMapBuilder.cs
@@ -11,6 +11,9 @@ public class TerrainHeightMapBuilder : MonoBehaviour {
 	
 	public Terrain terrain;
 	
+	private float[,] heights;
+	private float[,] errosion;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -19,8 +22,8 @@ public class TerrainHeightMapBuilder : MonoBehaviour {
     	var nRows = terrain.terrainData.heightmapResolution;
 		var nCols = terrain.terrainData.heightmapResolution;
 		float noiseScale = 0.02f;
-		var heights = new float[nRows, nCols];
-		
+		heights = new float[nRows, nCols];
+		errosion = new float[nRows, nCols];
 		
 		//var r = 50;
 		//int cX = 100;
@@ -28,7 +31,7 @@ public class TerrainHeightMapBuilder : MonoBehaviour {
 		
 		System.Random rnd = new System.Random();
 		
-		for(var i=0; i< 10;i++){
+		for(var i=0; i< 25;i++){
 		
 			int cX = rnd.Next(1, nRows); 
 			int cY = rnd.Next(1, nCols); 
@@ -36,55 +39,138 @@ public class TerrainHeightMapBuilder : MonoBehaviour {
 			heights = RaiseHill(heights,cX,cY,r);
 		}
 	
+		FindMinMax(heights);
+		
+		Erode(maxXIndex,maxYIndex);
+		
+		for(int i=0; i<nRows;i++){
+			for(int j=0; j<nRows;j++){
+				heights[i,j] = heights[i,j] - errosion[i,j];
+			}
+		}
 		
 		
-   	 	//for (var j = 0; j < nRows; j++) {
-       	//	for (var i = 0; i < nCols; i++) {
-		 //		heights[j,i] = RaiseHill(cX,cY,r,i,j);
-		//	}
-		//}
-		
-		
-		
-		//heights = RaiseHill(heights,cX,cY,r);
-		
-		//Step 3 normalise
-		//heights = Normalise(heights);
-		//Flatten(heights);
-		
+		Debug.Log(numIterations);
 		terrain.terrainData.SetHeights (0,0,heights);
 		
+	}
+	
+	
+	private List<float> riverPath = new List<float>(); 
+	
+	private int numIterations = 0;
+	
+	private void Erode(int startX, int startY) {
 		
-		/*
-		List<TreeInstance> treeInstances = new List<TreeInstance>(terrain.terrainData.treeInstances);
+		numIterations =	numIterations + 1;
 		
-		TreeInstance pTI = new TreeInstance();
+		//sample all the points around this point
+		//0 1 2
+		//3 4 5
+		//6 7 8
 		
-		float z = terrain.SampleHeight(new Vector3(250 , 250,100));
+		float[] surroundings = FindSurroundingPoints(heights,startX,startY);
 		
-	pTI.position = new Vector3(250 , 250,z);
+		int lowestIndex = GetIndexOfLowestPoint(surroundings);
+		
+		//lowestIndex = 1;
+		errosion[startX,startY] = 1f;
+		
+		int nextY;
+		int nextX;
+		
+		if(lowestIndex==4) {
+			
+			Debug.Log("FLAT OR LOWEST POINT");
+			//our current point is the local minima
+			//we need to do some digging.
+			
+			//lets continue the current gradient
+		} else {
+		
+			if(surroundings[lowestIndex]==OFF_MAP){
+				return;
+			}
+			
+			int y = lowestIndex/3;
+			int x = lowestIndex - 3*y;
+				
+			nextY = (startY -1)	+ y;
+			nextX = (startX -1)	+ x;
+			
+		//	Debug.Log(lowestIndex + " " + nextX + " " + nextY);
+			
+			Erode(nextX,nextY);
+		}
+		
+		//if(nextX<0 || 
+		
+		
+		//Erode (heights,nextX,nextY);
+			
+			
+		
+		
+	
+	}
+	
+	//private void ErodeNextPoint() {
+	//}
+	
 
-pTI.widthScale = 1;
+	const float EVEN_GRADIENT = 2000000;
+	
+	public int GetIndexOfLowestPoint(float[] surroundings){
+		//if we return -1 all the points have the same value.
+		int minIndex = 4;
+		float minValue = surroundings[4]; //this is the value of our current point (which is surrounded)
+		for(int i=0; i<surroundings.Length;i++){
+			if(surroundings[i]<=	minValue){
+				minValue = surroundings[i];
+				minIndex = i;
+			}
+		}
+				
+		return minIndex;
+	}
+	
+	public float[] FindSurroundingPoints(float[,] heights, int startX, int startY){
+		
+		float[] surroundings = new float[9];
+		
+		//0,1,2
+		//3,4,5
+		//6,7,8
+		
+		//where 4 is startX and startY
+		
+		surroundings[0] = GetPoint(heights, startX-1,startY-1); //0
+		surroundings[1] = GetPoint(heights, startX,startY-1); //1
+		surroundings[2] = GetPoint(heights, startX+1,startY-1); //2
+		
+		surroundings[3] = GetPoint(heights, startX-1,startY); //3
+		surroundings[4] = GetPoint(heights, startX,startY); //4
+		surroundings[5] = GetPoint(heights, startX+1,startY); //5
+		
+		surroundings[6] = GetPoint(heights, startX-1,startY+1); //6
+		surroundings[7] = GetPoint(heights, startX,startY+1); //5
+		surroundings[8] = GetPoint(heights, startX+1,startY+1); //8
+		
 
-pTI.heightScale = 1;
-
-pTI.color = Color.yellow;
-
-pTI.lightmapColor = Color.yellow;
-
-pTI.prototypeIndex = 0; //?
-
-//m_Terrain.AddTreeInstance(pTI);
+		return surroundings;
+	
+	}
+	
+	
 		
-		
-		
-		treeInstances.Add(pTI);
-		
-		
-		
-		//treeInstances = new 
-		
-		terrain.terrainData.treeInstances = treeInstances.ToArray();*/
+	const float OFF_MAP = 1000000;
+	
+	public float GetPoint(float[,] heights, int x, int y){
+		if(x<0 || x > (heights.GetLength(0)-1) || y<0 || y>(heights.GetLength(1)-1)){
+			return OFF_MAP;
+		} else {
+			return heights[x,y];
+		}
 	}
 	
 	private float[,] Flatten(float[,] heights){
@@ -101,9 +187,40 @@ pTI.prototypeIndex = 0; //?
 		return heights;
 	}
 	
+	//private 
+	
+	private float min;
+	private int maxXIndex;
+	private int maxYIndex;
+	private float max;
+	
+	private void FindMinMax(float[,] heights){
+		//float min = 0;
+		//float max =0;
+		float height = 0;
+		var nRows = terrain.terrainData.heightmapResolution;
+		var nCols = terrain.terrainData.heightmapResolution;
+		for (var j = 0; j < nRows; j++) {
+       		for (var i = 0; i < nCols; i++) {
+				height = heights[j,i];
+				if(height<=min) {
+					min = height;
+						
+				}
+				if(height>=max) {
+					max = height;
+					maxXIndex = j;
+						maxYIndex = i;
+				}
+			}
+		}
+	
+	}
+	
 	private float[,]  Normalise(float[,] heights){
-		float min = 0;
-		float max =0;
+	/*
+		//	float min = 0;
+	//	float max =0;
 		float height = 0;
 		var nRows = terrain.terrainData.heightmapResolution;
 		var nCols = terrain.terrainData.heightmapResolution;
@@ -117,7 +234,11 @@ pTI.prototypeIndex = 0; //?
 					max = height;
 				}
 			}
-		}
+		}*/
+		
+		float height = 0;
+		var nRows = terrain.terrainData.heightmapResolution;
+		var nCols = terrain.terrainData.heightmapResolution;
 		
 		for (var j = 0; j < nRows; j++) {
        		for (var i = 0; i < nCols; i++) {
